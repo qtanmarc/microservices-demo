8cef326df153436c1ba98fe885850fc6328bda41
Trong An Quang <taq@elca.vn>
2021-04-30 22:58:15 +0700

    Spring Boot app with Apache Kafka in Docker container https://habr.com/en/post/529222/

---
+++ kafka-example/src/main/java/com/example/demo/engine/Producer.java
@@ -0,0 +1,25 @@
+package com.example.demo.engine;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+/**
+ * @author taq
+ */
+@Service
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+public class Producer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public ListenableFuture<SendResult<String, String>> sendMessage(String topic, String key, String message) {
+        return this.kafkaTemplate.send(topic, key, message);
+    }
+
+}
---
+++ kafka-example/src/main/java/com/example/demo/tasks/SendMessageTask.java
@@ -0,0 +1,35 @@
+package com.example.demo.tasks;
+
+import java.time.LocalDate;
+import java.util.concurrent.ExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import com.example.demo.engine.Producer;
+
+/**
+ * @author taq
+ */
+@Component
+public class SendMessageTask {
+
+    private final Logger logger = LoggerFactory.getLogger(SendMessageTask.class);
+    private Producer producer;
+
+    public SendMessageTask(Producer producer) {
+        this.producer = producer;
+    }
+
+    @Scheduled(fixedRateString = "3000")
+    public void send() throws ExecutionException, InterruptedException {
+        ListenableFuture<SendResult<String, String>> listenableFuture = this.producer.sendMessage("INPUT_DATA", "IN_KEY",
+            LocalDate.now().toString());
+        SendResult<String, String> result = listenableFuture.get();
+        logger.info(String.format("Produced:\ntopic: %s\noffset: %d\npartition: %d\nvalue size: %d", result.getRecordMetadata().topic(),
+            result.getRecordMetadata().offset(),
+            result.getRecordMetadata().partition(), result.getRecordMetadata().serializedValueSize()));
+    }
+}
